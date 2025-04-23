c40da3c877c79c91093a7453dcae68203cc11bea
XxXmishkaXxX <misha.kutsov1@gmail.com>
2025-04-23 19:30:00 +0500

    add producer

---
+++ generator/kafka/producer.py
@@ -0,0 +1,33 @@
+import json
+from aiokafka import AIOKafkaProducer
+
+from generator.core.config import settings
+
+class Producer:
+    def __init__(self, topic: str, bootstrap_server: str):
+        self.topic = topic
+        self.server = bootstrap_server
+        self.producer = AIOKafkaProducer(bootstrap_servers=self.server)
+
+    async def start(self):
+        await self.producer.start()
+
+    async def stop(self):
+        await self.producer.stop()
+
+
+    async def send_image(self, user_id: int, image_string: str):
+
+        message = {
+            "user_id": user_id,
+            "image_base64": image_string
+        }
+
+        await self.producer.send_and_wait(
+            self.topic,
+            json.dumps(message).encode("utf-8")
+        )
+
+
+producer = Producer(topic=settings.TOPIC_RESPONSE, 
+                    bootstrap_server=settings.BOOTSTRAP_SERVERS)
