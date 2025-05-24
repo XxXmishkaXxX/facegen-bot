f1f9ba07623d31af6a52f34c0de5d25bca1e487a
XxXmishkaXxX <misha.kutsov1@gmail.com>
2025-05-24 16:56:31 +0500

    add new method for faseswap

--- bot/kafka/producer.py
+++ bot/kafka/producer.py
@@ -16,10 +16,24 @@ class Producer:
     async def stop(self):
         await self.producer.stop()
 
+    async def send_faceswap_request(self, user_id, source_base64, target_base64):
+        message = {
+            "user_id": user_id,
+            "event": "face_swap",
+            "source_photo": source_base64,
+            "target_photo": target_base64
+        }
+
+        await self.producer.send_and_wait(
+            self.topic,
+            json.dumps(message).encode("utf-8")
+        )
+
 
     async def send_request(self, user_id: int, prompt: str, model: str, negative_prompt: str):
         message = {
             "user_id": user_id,
+            "event": "face_generate",
             "prompt": prompt,
             "negative_prompt": negative_prompt,
             "model_name": model
