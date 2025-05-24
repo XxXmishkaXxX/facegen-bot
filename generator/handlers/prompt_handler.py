256688a8c44a208e45e9d2a374595b1f04d64ae9
XxXmishkaXxX <misha.kutsov1@gmail.com>
2025-05-24 16:58:10 +0500

    ref prompt handler

--- generator/handlers/prompt_handler.py
+++ generator/handlers/prompt_handler.py
@@ -1,14 +1,24 @@
 from core.logger import logger
 from kafka.producer import producer
-from models.model_manager import get_model_manager
+from models.model_manager import get_models
 
-mm = get_model_manager()
+fs, fg = get_models()
 
-async def handle_prompt(user_id: int, prompt: str, model_name: str, negative_prompt: str):
+async def handle_prompt(user_id: int, event: str, **kwargs):
     try:
-        logger.info(f"Получен запрос на генерацию изображения для пользователя {user_id} с запросом: {prompt}")
+        if event == "face_swap":
+            source_path = kwargs.get("source_photo")
+            target_path = kwargs.get("target_photo")
+
+            if not source_path or not target_path:
+                raise ValueError("Необходимо указать source_path и target_path для face_swap")
+
+            image_str = fs.swap_faces(source_path, target_path)
         
-        image_str = await mm.async_generate_image(prompt, model_name, negative_prompt)
+        elif event == "face_generate":
+            prompt = kwargs.get("prompt")
+            negative_prompt = kwargs.get("negative_prompt")
+            image_str = await fg.async_generate_image(prompt, negative_prompt)
         
         await producer.send_image(user_id=user_id, image_string=image_str)
         
